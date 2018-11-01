fd1198e9d2ed9a7ff236a22fd8e5d57cc9e9b9e9
pr1malbyt3s <al33w6@gmail.com>
2018-11-01 11:36:51 -0400

    Update Pass_Gen.py

--- Pass_Gen.py
+++ Pass_Gen.py
@@ -8,9 +8,9 @@ numbers_count = input("Enter number of desired digits in password.")
 letters_count = input("Enter number of desired letters in password.")
 special_count = input("Enter number of desired special characters in password.")
 
-numbers = random.choice(string.digits) for i in range(numbers_count)
-letters = random.choice(string.letters) for in in range(letters_count)
-special = random.choice(string.punctuation) for i in range(special_count)
+numbers = (random.choice(string.digits)) for x in range(numbers_count)
+letters = random.choice(string.letters) for x in range(letters_count)
+special = random.choice(string.punctuation) for x in range(special_count)
 
 password = numbers + letters + special
 
