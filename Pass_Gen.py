3451312835f843dcef2e93f85c0732d3b298963f
pr1malbyt3s <al33w6@gmail.com>
2018-11-01 12:02:34 -0400

    Update Pass_Gen.py

--- Pass_Gen.py
+++ Pass_Gen.py
@@ -3,15 +3,35 @@
 import random
 import string
 
+#Input validation function that enforces user to supply integer values between 0 and 10 for counts.
+def input_validate(prompt):
+  while True:
+    try:
+      value = int(input(prompt))
+    except ValueError:
+      print("Input could not be interpreted. Try again.")
+      continue
+    if value < 0 or value > 10:
+      print("Supplied number must be between 0 and 10.")
+      continue
+    else:
+      break
+  return value
+
 #Assign desired number of digits, letters, and special characters.
-numbers_count = input("Enter number of desired digits in password.")
-letters_count = input("Enter number of desired letters in password.")
-special_count = input("Enter number of desired special characters in password.")
+numbers_count = input_validate("Enter number of desired digits in password: ")
+letters_count = input_validate("Enter number of desired letters in password: ")
+special_count = input_validate("Enter number of desired special characters in password: ")
 
-numbers = (random.choice(string.digits)) for x in range(numbers_count)
-letters = random.choice(string.letters) for x in range(letters_count)
-special = random.choice(string.punctuation) for x in range(special_count)
+#Generate randomly assigned, desired length lists of numbers, letters, and special characters.
+numbers = ''.join((random.choice(string.digits)) for i in range(numbers_count))
+letters = ''.join((random.choice(string.letters)) for in in range(letters_count))
+special = ''.join((random.choice(string.punctuation)) for i in range(special_count))
 
+#Combine randomly generated lists into a single entity and assign password length variable.
 password = numbers + letters + special
+password_length = numbers_count + letters_count + special_count
 
-print ''.join(random.choice(password))
+#Print generated password to STDOUT.
+print ("Your generated password is: ")
+print ''.join((random.choice(password)) for i in range(password_lenght))
