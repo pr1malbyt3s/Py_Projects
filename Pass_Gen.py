49e8bb4682ef4c5d3fee12ea920f1625bc057423
pr1malbyt3s <al33w6@gmail.com>
2018-11-01 11:34:56 -0400

    Update Pass_Gen.py

--- Pass_Gen.py
+++ Pass_Gen.py
@@ -3,6 +3,15 @@
 import random
 import string
 
-numbers = string.digits
-letters = string.letters
-special = string.punctuation
+#Assign desired number of digits, letters, and special characters.
+numbers_count = input("Enter number of desired digits in password.")
+letters_count = input("Enter number of desired letters in password.")
+special_count = input("Enter number of desired special characters in password.")
+
+numbers = random.choice(string.digits) for i in range(numbers_count)
+letters = random.choice(string.letters) for in in range(letters_count)
+special = random.choice(string.punctuation) for i in range(special_count)
+
+password = numbers + letters + special
+
+print ''.join(random.choice(password))
