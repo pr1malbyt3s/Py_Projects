5345aac8d2c01eb86e9e41ea6026c90fdf4fb893
pr1malbyt3s <al33w6@gmail.com>
2018-11-30 17:17:10 -0500

    Update so_lost.py

--- so_lost.py
+++ so_lost.py
@@ -10,16 +10,16 @@ port = 31802
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-while True:
-    data = s.recv(1024).decode('utf-8')
-    print(data)
-    if "\nup" in data:
-      s.send("^".encode('utf-8'))
-    elif "\ndown" in data:
-      s.send("V".encode('utf-8'))
-    elif "\nleft" in data:
-      s.send("<".encode('utf-8'))
-    elif "\nright" in data:
-      s.send(">".encode('utf-8'))
+#while True:
+data = s.recv(1024).decode('utf-8')
+print(data)
+if "\nup" in data:
+  s.send("^".encode('utf-8'))
+elif "\ndown" in data:
+  s.send("V".encode('utf-8'))
+elif "\nleft" in data:
+  s.send("<".encode('utf-8'))
+elif "\nright" in data:
+  s.send(">".encode('utf-8'))
 # Close connection
 s.close()
