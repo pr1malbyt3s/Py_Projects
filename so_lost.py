764e37cbeba460a87baa398df5be401dc9b81cae
pr1malbyt3s <al33w6@gmail.com>
2018-11-30 16:39:06 -0500

    Update so_lost.py

--- so_lost.py
+++ so_lost.py
@@ -1,27 +1,28 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env/python
 
 import socket
- 
+import sys
+  
 host = '52.61.142.146'
 port = 31802
  
 # Create socket and connect
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-while 1:
-# Receives up to 1024 bytes
- data = s.recv(1024)
- print data
- stringdata = data.decode('utf-8')
- if "/nup" in stringdata:
-  s.send(b"^")
- elif "/ndown" in stringdata:
-  s.send(b"V")
- elif "/nleft" in stringdata:
-  s.send(b"<")
- elif "/nright" in stringdata:
-  s.send(b">")
+try:
+ while 1:
+  data = s.recv(1024)
+  print data
+  stringdata = data.decode('utf-8')
+  if "/nup" in stringdata:
+   s.send(b"^")
+  elif "/ndown" in stringdata:
+   s.send(b"V")
+  elif "/nleft" in stringdata:
+   s.send(b"<")
+  elif "/nright" in stringdata:
+   s.send(b">")
  
 # Close connection
- s.close()
+s.close()
