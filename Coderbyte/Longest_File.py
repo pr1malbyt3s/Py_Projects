56188b63f714869b80c0741731c303afe0765377
pr1malbyt3s <aaronwilliams.infosec@gmail.com>
2021-06-11 16:39:02 -0400

    Create Longest_File.py

---
+++ Coderbyte/Longest_File.py
@@ -0,0 +1,17 @@
+# The purpose of this challenge was to identify the largest word in a supplied sentence.
+# Words could contain numbers but not special characters.
+# It was assumed the supplied sentence would not be empty.
+
+# Import the regex library:
+import re
+
+def LongestWord(sen):
+  # Set the regex pattern for anything not a letter or number:
+  regex = re.compile('[^a-zA-Z0-9]+')
+  # Create the list of parsed words from the sentence:
+  parsed = [re.sub(regex, "", i) for i in sen.split()]
+  # Find the largest word in the parsed list using the length as the key:
+  return max(parsed, key=len)
+
+# keep this function call here 
+print(LongestWord(input()))
