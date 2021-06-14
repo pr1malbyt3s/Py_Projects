68a29dbe24d3147726fb2cedf173d888fd3d87e8
pr1malbyt3s <aaronwilliams.infosec@gmail.com>
2021-06-14 11:27:22 -0400

    Create First_Factorial.py

---
+++ Coderbyte/First_Factorial.py
@@ -0,0 +1,12 @@
+# The goal of this challenge was to simply find the factorial value of a supplied number.
+# This is a simple solution using the math library.
+
+# Import math library:
+import math
+
+def FirstFactorial(num):
+  # Return the factorial for the supplied num parameter:
+  return math.factorial(num)
+
+# keep this function call here 
+print(FirstFactorial(input()))
